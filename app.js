fd4840538be458aee6bb238f61413eecb09f1986
Flavio <flavio_125@hotmail.com>
2023-08-31 16:45:08 -0300

    Adicionando manipulador de eventos...

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    let className = document.body.className;
+    if (className == "ligth-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "ligth";
+    }
+});
